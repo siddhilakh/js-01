8631c5e5e630d63a9e9474e1f8932c45df44f585
siddhilakh <87911678+siddhilakh@users.noreply.github.com>
2026-03-23 15:52:50 +0000

    comparison

---
+++ .devcontainer/01_basics/04_comparision.js
@@ -0,0 +1,6 @@
+//console.log(null > 0)
+//console.log(null == 0)
+//console.log(null >= 0)
+// equality check (==) and comparisons(like <,>,>=,etc) are treated differently
+// comparisons convert null to a number treating it as zero
+console.log("2" == 2)
