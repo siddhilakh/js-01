a97de4fc4a389f2d7d17b086f570ef06955704ff
siddhilakh <87911678+siddhilakh@users.noreply.github.com>
2026-03-25 17:04:29 +0000

    num and maths

---
+++ .devcontainer/01_basics/06_nums_and_maths.js
@@ -0,0 +1,26 @@
+const score = 400
+//console.log(score);
+const balance = new Number(100)
+//console.log(balance)
+//console.log(balance.toString().length)
+//console.log(balance.toFixed(2))
+const otherNumber = 23.45678
+//console.log(otherNumber.toPrecision(4));
+const hundreds = 100000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+
+//**********************************MATHS**************************************************
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(3.8));
+// console.log(Math.ceil(3.8));
+// console.log(Math.floor(3.8));
+console.log(Math.random())
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
